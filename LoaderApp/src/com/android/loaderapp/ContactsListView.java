2aee0983597c8b0e65766c4b278d4b1c7d13605f
Jeff Hamilton <jham@android.com>
2010-04-08 16:26:36 -0500

    Initial take of the LoaderApp.
    This is an experiment in splitting apps
    into components that are finer grained
    than Activities so that the components
    can be mixed and matched on large screens.
    
    The code is lossely based on the Contacts
    application, reusing much of the logic
    for displaying contact details and none
    of the logic for the contacts list.
    
    Change-Id: I74f1c438184cf260e03fa3f2fe7a2fee395d9dbc

---
+++ LoaderApp/src/com/android/loaderapp/ContactsListView.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.loaderapp;
+
+import com.android.loaderapp.model.VisibleContactsLoader.ListQuery;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract.Contacts;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ContactsListView extends ListView {
+    ContactsListViewFactory mViewFactory;
+
+    public ContactsListView(Context context) {
+        this(context, null);
+    }
+
+    public ContactsListView(Context context, AttributeSet attrs) {
+        this(context, attrs, com.android.internal.R.attr.listViewStyle);
+    }
+
+    public ContactsListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        setAdapter(new Adapter(context));
+    }
+
+    /**
+     * Build the {@link Contacts#CONTENT_LOOKUP_URI} for the given
+     * {@link ListView} position, using {@link #mAdapter}.
+     */
+    public Uri getContactUri(int position) {
+        if (position == ListView.INVALID_POSITION) {
+            throw new IllegalArgumentException("Position not in list bounds");
+        }
+
+        final Cursor cursor = (Cursor) getAdapter().getItem(position);
+        if (cursor == null) {
+            return null;
+        }
+
+        // Build and return soft, lookup reference
+        final long contactId = cursor.getLong(ListQuery.COLUMN_ID);
+        final String lookupKey = cursor.getString(ListQuery.COLUMN_LOOKUP_KEY);
+        return Contacts.getLookupUri(contactId, lookupKey);
+    }
+
+    
+    public interface ContactsListViewFactory {
+        public View newView(Context context, ViewGroup parent);
+        public void bindView(View view, Context context, Cursor cursor);
+    }
+
+    public void setViewFactory(ContactsListViewFactory factory) {
+        mViewFactory = factory;
+    }
+
+    /** Sets the cursor that the list displays */
+    public void setCursor(Cursor cursor) {
+        Adapter adapter = (Adapter) getAdapter();
+        adapter.changeCursor(cursor);
+    }
+
+    /**
+     * A simple view factory that inflates the views from XML and puts the display
+     * name in @id/name.
+     */
+    public static class SimpleViewFactory implements ContactsListViewFactory {
+        private int mResId;
+
+        public SimpleViewFactory(int resId) {
+            mResId = resId;
+        }
+
+        public View newView(Context context, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            return inflater.inflate(mResId, parent, false);
+        }
+
+        public void bindView(View view, Context context, Cursor cursor) {
+            TextView name = (TextView) view.findViewById(R.id.name);
+            name.setText(cursor.getString(ListQuery.COLUMN_NAME));
+        }
+    }
+    
+    private class Adapter extends CursorAdapter {
+        public Adapter(Context context) {
+            super(context, null, 0);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            View view = mViewFactory.newView(context, parent);
+            mViewFactory.bindView(view, context, cursor); 
+            return view;
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            mViewFactory.bindView(view, context, cursor);
+        }
+    }
+}
---
+++ LoaderApp/src/com/android/loaderapp/HomeNormal.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.loaderapp;
+
+import com.android.loaderapp.ContactsListView.SimpleViewFactory;
+import com.android.loaderapp.model.VisibleContactsLoader;
+
+import android.app.patterns.CursorLoader;
+import android.app.patterns.Loader;
+import android.app.patterns.LoaderActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class HomeNormal extends LoaderActivity<Cursor> implements OnItemClickListener {
+    static final int LOADER_LIST = 1;
+
+    ContactsListView mList;
+    CursorLoader mLoader;
+
+    @Override
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+
+        setContentView(R.layout.normal_home);
+
+        mList = (ContactsListView) findViewById(android.R.id.list);
+        mList.setViewFactory(new SimpleViewFactory(R.layout.normal_list_item));
+        mList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onInitializeLoaders() {
+        startLoading(LOADER_LIST, null);
+    }
+
+    @Override
+    protected Loader onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case LOADER_LIST:
+                return new VisibleContactsLoader(this);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadComplete(int id, Cursor data) {
+        switch (id) {
+            case LOADER_LIST:
+                mList.setCursor(data);
+                break;
+        }
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // The user clicked on an item in the the list, start an activity to view it
+        Uri contactUri = mList.getContactUri(position);
+        if (contactUri != null) {
+            Intent intent = new Intent(this, DetailsNormal.class);
+            intent.setData(contactUri);
+            startActivity(intent);
+        }
+    }
+}
