3f4f1b76c1b6238a59e02ecf2e0a993db565307d
Erik <epastern@google.com>
2011-03-24 13:07:53 -0700

    Second canary version (v14)
    Included more verbose logging, fixes, and updates to target all
    devices.
    
    Change-Id: I83c683c259db44ab138986fc2bf1ab37d4204705

--- AndroidVendorSecurityTool/src/com/android/vending/sectool/v1/BackendTest.java
+++ AndroidVendorSecurityTool/src/com/android/vending/sectool/v1/BackendTest.java
@@ -4,30 +4,53 @@ package com.android.vending.sectool.v1;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.zip.CRC32;
 
 public class BackendTest {
 
-    public static boolean profileExists() {
-        File f = new File("/system/bin/profile");
+    public static boolean profileExists(File f) {
         return f.exists();
+    }
+    
+    public static boolean isImmunized(File f) {
+        if (GoogleSecurityToolActivity.DEBUG)
+            Log.d("AVST", "length is " + f.length());
+        return f.length() == 0;
+    }
+    
+    public static long profSize(File f) {
+        return f.length();
+    }
+    
+    public static boolean crcMatches(File f) {
+        if (GoogleSecurityToolActivity.DEBUG)
+            Log.d("AVST", "Getting checksum");
+        return getChecksum(f) == 1911844080;
     }
 
     public static String runRemovalCommand(Context context) {
         StringBuffer output = new StringBuffer();
+        InputStream in = null;
+        OutputStream os = null;
         try {
             AssetManager am = context.getAssets();
-            InputStream in = am.open("droiddreamclean");
+            in = am.open("droiddreamclean");
             output.append("aa");
-            String location = "/data/data/" + context.getPackageName() + "/droiddreamclean";
-            OutputStream os = new FileOutputStream(location);
+            File location = context.getFileStreamPath("droiddreamclean");
+            os = new FileOutputStream(location);
             output.append("bb");
             byte[] buff = new byte[16000];
             int read;
@@ -38,10 +61,10 @@ public class BackendTest {
             in.close();
             output.append("cc");
             Runtime rt = Runtime.getRuntime();
-            rt.exec("/system/bin/chmod 755 " + location);
+            rt.exec("/system/bin/chmod 755 " + location.toString());
             output.append("dd");
             Process process = rt.exec("/system/bin/profile -c "
-                    + location);
+                    + location.toString());
             output.append("ee");
 
 // String removalCommand = "/system/bin/profile -c ls /";
@@ -58,10 +81,40 @@ public class BackendTest {
 
             process.waitFor();
         } catch (Exception e) {
-            Log.d(GoogleSecurityToolActivity.TAG, e.getMessage());
+            if (GoogleSecurityToolActivity.DEBUG)
+                Log.d(GoogleSecurityToolActivity.TAG, e.getMessage());
             output.append(e.getMessage());
+        } finally {
+//            if (os != null) {
+//                os.close();
+//            }
+//            if (in != null) {
+//                in.close();
+//            }
         }
         return output.toString();
+    }
+    
+    public static long getChecksum(File f) {
+        try {
+            FileInputStream in = new FileInputStream(f);
+            CRC32 crc = new CRC32();
+            byte[] bytes = new byte[8192];
+            int byteCount;
+            crc.reset();
+            while ((byteCount = in.read(bytes)) > 0) {
+              crc.update(bytes, 0, byteCount);
+            }
+            in.close();
+            long sum = crc.getValue();
+            if (GoogleSecurityToolActivity.DEBUG)
+                    Log.d("AVST", "crc is --" + sum + "--");
+            return sum;
+     
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 
 }
