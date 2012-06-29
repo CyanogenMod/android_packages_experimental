66401a2cc8bfe9b8605d8383f8970947840c4de2
Chris Wren <cwren@android.com>
2012-06-29 15:46:22 -0400

    Port notification showcase to android-support-v4.
    Includes OS-appropriate iconography for pre-Gingerbread,
    Gingerbread, and HC+, thanks to Android Asset Studio.
    
    Bug: 6316809
    Change-Id: I348c0951a0329ac6ad65bf9838781c8e2b0c9397

--- NotificationShowcase/Android.mk
+++ NotificationShowcase/Android.mk
@@ -24,6 +24,8 @@ LOCAL_AAPT_FLAGS += -c mdpi,hdpi,xhdpi
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+
 LOCAL_PACKAGE_NAME := NotificationShowcase
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
