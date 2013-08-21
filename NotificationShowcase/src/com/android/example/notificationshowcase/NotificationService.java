8561d489822fdeef39519058501b4a3980f92255
Chris Wren <cwren@android.com>
2013-08-21 16:43:55 -0400

    update notificaiton showcase with heads up features
    phone call is ongoing
    add functional actions to the phone call that dismiss it
    add a fullscreenintent intent to the phone call
    
    This was dropped in a bad merge.
    Original-Change-Id: Ie6a1a720ebb585891071410fabbf410d92dddcdd
    
    Change-Id: I0bf53e8686d9be29283c5d5daa74ebede9939d88

--- NotificationShowcase/src/com/android/example/notificationshowcase/NotificationService.java
+++ NotificationShowcase/src/com/android/example/notificationshowcase/NotificationService.java
@@ -127,6 +127,8 @@ public class NotificationService extends IntentService {
         long uploadWhen = System.currentTimeMillis();
         mNotifications.add(makeUploadNotification(this, 10, uploadWhen));
 
+        int phoneId = mNotifications.size();
+        final PendingIntent fullscreenIntent = FullScreenActivity.getPendingIntent(this, phoneId);
         Notification phoneCall = new NotificationCompat.Builder(this)
                 .setContentTitle("Incoming call")
                 .setContentText("Matias Duarte")
@@ -134,14 +136,14 @@ public class NotificationService extends IntentService {
                 .setSmallIcon(R.drawable.stat_sys_phone_call)
                 .setDefaults(Notification.DEFAULT_SOUND)
                 .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setContentIntent(ToastService.getPendingIntent(this, "Clicked on Matias"))
+                .setContentIntent(fullscreenIntent)
+                .setFullScreenIntent(fullscreenIntent, true)
                 .addAction(R.drawable.ic_dial_action_call, "Answer",
-                        ToastService.getPendingIntent(this, "call answered"))
+                        PhoneService.getPendingIntent(this, phoneId, PhoneService.ACTION_ANSWER))
                 .addAction(R.drawable.ic_end_call, "Ignore",
-                        ToastService.getPendingIntent(this, "call ignored"))
-                .setAutoCancel(true)
+                        PhoneService.getPendingIntent(this, phoneId, PhoneService.ACTION_IGNORE))
+                .setOngoing(true)
                 .build();
-        phoneCall.flags |= Notification.FLAG_INSISTENT;
         mNotifications.add(phoneCall);
 
         mNotifications.add(new NotificationCompat.Builder(this)
