e182ffc192437131e5f5efb5b7d5be2cd792c180
Mukarram Tariq <mukarram@google.com>
2014-01-31 10:25:35 -0800

    Copy screenshot data to proto.
    Bug: b/12805843
    Change-Id: Id565ac42cd85d4b58b3482fcf59fae7c01d3d5df

--- PixelPerfect/tests/src/com/google/android/apps/pixelperfect/ScreenshotGrabberTest.java
+++ PixelPerfect/tests/src/com/google/android/apps/pixelperfect/ScreenshotGrabberTest.java
@@ -3,8 +3,12 @@ package com.google.android.apps.pixelperfect;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.view.Surface;
 
+import com.google.common.logging.RecordedEvent;
+import com.google.common.logging.RecordedEvent.Screenshot;
 
 import junit.framework.TestCase;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Tests for {@link ScreenshotGrabber}.
@@ -27,5 +31,32 @@ public class ScreenshotGrabberTest extends TestCase {
         assertEquals(270f, mGrabber.getDegreesForRotation(Surface.ROTATION_90), FLOAT_DELTA);
         assertEquals(180f, mGrabber.getDegreesForRotation(Surface.ROTATION_180), FLOAT_DELTA);
         assertEquals(90f, mGrabber.getDegreesForRotation(Surface.ROTATION_270), FLOAT_DELTA);
+    }
+
+    public void testFillScreenshotProto() throws Exception {
+        final int height = 10;
+        final int width = 15;
+        final int rotation = 100;
+        final int quality = 50;
+        final byte[] bytes = new byte[] { (byte) 0xca, (byte) 0xfe };
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream(bytes.length);
+        output.write(bytes, 0, bytes.length);
+        Screenshot screenshot = ScreenshotGrabber.fillScreenshotProto(
+                output, height, width, rotation,
+                RecordedEvent.Bitmap.BitmapConfig.Config.ARGB_8888,
+                RecordedEvent.Bitmap.CompressionConfig.CompressFormat.JPEG,
+                quality);
+
+        assertEquals(bytes, screenshot.getBitmap().getBitmap());
+        assertEquals(height, screenshot.getBitmap().getHeight());
+        assertEquals(width, screenshot.getBitmap().getWidth());
+        assertEquals(rotation, screenshot.getRotation());
+
+        assertEquals(RecordedEvent.Bitmap.BitmapConfig.Config.ARGB_8888,
+                screenshot.getBitmap().getBitmapConfig().getValue());
+        assertEquals(RecordedEvent.Bitmap.CompressionConfig.CompressFormat.JPEG,
+                screenshot.getBitmap().getCompressionConfig().getFormat());
+        assertEquals(quality, screenshot.getBitmap().getCompressionConfig().getQuality());
     }
 }
