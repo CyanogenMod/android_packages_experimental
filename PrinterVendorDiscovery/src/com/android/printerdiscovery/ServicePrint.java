d6e853e25385ac79db0e0361cff24ef3a42b8cf9
Philip P. Moltmann <moltmann@google.com>
2016-02-04 15:58:57 -0800

    PrinterDiscovery: Add License
    Change-Id: I57afd05e111359b2949e225aa60e1dcaed306c0e

--- PrinterVendorDiscovery/src/com/android/printerdiscovery/ServicePrint.java
+++ PrinterVendorDiscovery/src/com/android/printerdiscovery/ServicePrint.java
@@ -1,5 +1,6 @@
 /*
  * (c) Copyright 2016 Mopria Alliance, Inc.
+ * (c) Copyright 2016 Mopria Alliance, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
